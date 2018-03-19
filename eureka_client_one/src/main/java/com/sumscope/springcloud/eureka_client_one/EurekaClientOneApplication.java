b908aa68c42d5e44e7a3aff3334ea2812bb047b1
xuejian.sun <xuejian.sun@sumscope.com>
2018-03-19 13:27:13 +0800

    eureak

---
+++ eureka_client_one/src/main/java/com/sumscope/springcloud/eureka_client_one/EurekaClientOneApplication.java
@@ -0,0 +1,14 @@
+package com.sumscope.springcloud.eureka_client_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientOneApplication.class, args);
+	}
+}
